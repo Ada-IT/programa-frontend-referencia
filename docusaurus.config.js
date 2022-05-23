32782620abcf65b1261417fd4c48463a39226081
Ezequiel Gonzalez <ezequiel.gonzalez@verishop.com>
2022-05-23 14:05:18 -0300

    update programa referencia frontend

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,85 +1,78 @@
-module.exports = {
-  title: 'Desarrollo Frontend',
-  tagline: 'Programa',
-  url: 'https://ada.com.ar',
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Desarollo Frontend',
+  tagline: 'ADA ITW',
+  url: 'https://adaitw.org',
   baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/ada-favicon.png',
-  organizationName: 'AdaIt', // Usually your GitHub org/user name.
-  projectName: 'adadocs', // Usually your repo name.
-  themeConfig: {
-    prism: {
-      theme: require('prism-react-renderer/themes/palenight'),
-    },
-    navbar: {
-      title: 'Ada',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/iso_blanco.svg',
-      },
-      items: [
-        { to: 'docs/modulo-1/clase-01', label: 'Clases', position: 'left' },
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Frontend',
-              to: 'https://frontend.adaitw.org/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/',
-            },
-          ],
-        },
-        {
-          title: 'Social',
-          items: [
-            {
-              label: 'Linkedin',
-              href: 'https://www.linkedin.com/school/ada-itw/',
-            },
-            {
-              label: 'Instagram',
-              href: 'https://instagram.com/adaitw',
-            },
-            {
-              label: 'Facebook',
-              href: 'https://www.facebook.com/adaitw/',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Ada Itw.`,
-    },
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Ada ITW', // Usually your GitHub org/user name.
+  projectName: 'Desarollo Frontend', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
+      }),
     ],
   ],
-}
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Ada ITW',
+        logo: {
+          alt: 'Ada ITW',
+          src: 'img/iso_blanco.svg',
+        },
+        items: [
+          {
+            to: 'docs/modulo-1/clase-01',
+            label: 'Clases',
+            position: 'left',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Ada ITW.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
